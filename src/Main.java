265f1e192fcd79219b2cbda7fa0102c3a630739e
christianekblad <christian.ekblad.mailbox@gmail.com>
2021-02-05 11:05:06 +0100

    added decrement method

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,19 @@ public class Main {
         int increment = increment(num);
         System.out.println("after increment: " +increment);
 
+        int number = 10;
+        System.out.println("\nbefore decrement: " +number);
+
+        int decrement = decrement(number);
+        System.out.println("after decrement: " +decrement);
+
     }
 
     public static int increment(int number) {
         return ++number;
+    }
+
+    public static int decrement(int number) {
+        return --number;
     }
 }
