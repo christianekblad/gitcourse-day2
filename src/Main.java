793d3a46ea1f19e8c6d63b45416ea5a99338d7ea
christianekblad <christian.ekblad.mailbox@gmail.com>
2021-02-05 09:50:56 +0100

    added some code

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 	    int num = 5;
+        System.out.println("before increment: " +num);
+
         int increment = increment(num);
-        System.out.println(increment);
+        System.out.println("after increment: " +increment);
+
     }
 
     public static int increment(int number) {
